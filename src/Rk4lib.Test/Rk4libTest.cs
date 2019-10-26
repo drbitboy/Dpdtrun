8b449e84f9e3422859d76183fa521d711db84b22
Brian Carcich <BrianTCarcich@gmail.com>
2019-10-26 08:43:54 -0400

    clean up c# code, add carriage returns

--- src/Rk4lib.Test/Rk4libTest.cs
+++ src/Rk4lib.Test/Rk4libTest.cs
@@ -8,52 +8,50 @@ namespace Rk4lib.Test
     // Runge-Kutta fourth-order method (Rk4lib) tests
     ////////////////////////////////////////////////////////////////////
  
-    public class ParabolaRk4 : Rk4     // Sub-class from Rk4lib.Rk4 base class
+    public class ParabolaRk4 : Rk4  // Sub-class from Rk4lib.Rk4 abstract base class
     {
         ////////////////////////////////////////////////////////////////
         //                  2       1      0
-        // Parabola:  y = ax   +  bx  +  cx
+        // Parabola:  y = Ax   +  Bx  +  Cx
         //
-        //      and:  dy/dx = 2ax + b
+        //      and:  dy/dx = 2Ax + B
+        //
         ////////////////////////////////////////////////////////////////
 
         // Parameters
-        // c does not matter for testing as it is a fixed offset for y
         public double A { set; get; }
         public double B { set; get; }
+        // C does not matter for testing as it is a fixed offset for y
 
         // Parabola dy/dx model
         public override double dydx(double x, double y)
         {
-            double dydx = (2d * A * x) + B;  // dy/dx = 2ax + b
-            return dydx;
+            return (2d * A * x) + B;
         }
     }
     [TestClass]
     public class Rk4libParabolaTest
     {
 
-
         [TestMethod]
         public void ParabolaForward20()
         {
-            // Parabola with a=2 and b=0
+            // Parabola with A=2 and B=0
             ParabolaRk4 rk4 = new ParabolaRk4() { A = 2d, B = 0d };
 
             // Setup Rk4 intgration from x=1 to x=10
             double x0 = 1d;                         // Initial x
             double y0 = (rk4.A * x0 * x0) + rk4.B;  // Initial y
             double h = 1d;                          // Step size
-            double x = x0 + 8.9;                    // Final x
+            double x = x0 + 8.9;                    // Minimum final x
 
             // Integrate Rk4 algorithm stops at (x0 + N*h),
             // with N integer and N>=8.9
             List<List<double>> result = rk4.Rk4_solve(x0, y0, x, h);
 
-            // Check lists' sizes
-            Assert.AreEqual(2, result.Count);
-            Assert.AreEqual(10, result[1].Count);
+            Assert.AreEqual(2, result.Count);      // Check lists' sizes
             Assert.AreEqual(10, result[0].Count);
+            Assert.AreEqual(10, result[1].Count);
 
             // Check values
             for (int i = 0; i < 10; ++i)
@@ -71,14 +69,13 @@ namespace Rk4lib.Test
         [TestMethod]
         public void Parabola100ReverseNeg28()
         {
-            // Test Parabola with a=-2, b=8, and negative step size
+            // Test Parabola with A=-2, B=8, and negative, non-unity step size
             ParabolaRk4 rk4 = new ParabolaRk4() { A = -2d, B = 8d };
 
             // Initial x0=100, y0=-19200;  h=-2, stop after fourth step
             List<List<double>> result = rk4.Rk4_solve(100d, -19200d, 100d-7.9d, -2d);
 
-            // Check lists' sizes
-            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(2, result.Count);     // Check lists' sizes
             Assert.AreEqual(5, result[1].Count);
             Assert.AreEqual(5, result[0].Count);
 
--- src/Rk4lib/Rk4lib.cs
+++ src/Rk4lib/Rk4lib.cs
@@ -6,46 +6,67 @@ using System.Threading.Tasks;
 
 namespace Rk4lib
 {
-
+    // Simple abstract Runge-Kutta 4th-order integration model
     public abstract class Rk4
     {
+        // Abstract method, which will be overridden, to calculate dy/dx derivative
+        // - Concrete class will typically also contain parameters used by the concrete method
         public abstract double dydx(double x, double y);
 
+        /// <summary>
+        /// Fourth-order Runge-Kutta algorithm to integrate y from x0 to x; O((x-x0)/h)
+        /// </summary>
+        /// <param name="x0">Inital value (condition) of independent variable</param>
+        /// <param name="y0">Initial value (condition) of dependent variable</param>
+        /// <param name="x">Final value of independent variable</param>
+        /// <param name="h">Step size for x; h&gt;0 if x&gt;x0; h&lt;0 if x&lt0; h!=0</param>
+        /// <returns>
+        /// List&lt;List&lt;double&gt;&gt;
+        /// - List of two lists
+        ///   - First list contains x values:  [x0, x0+h, x0+2h, ..., xN]; N = floor((x-x0)/h)
+        ///     - x <= xN < x+h
+        ///   - Second list contains y values:  [y0, y1, y2, ..., yN]
+        /// </returns>
         public List<List<double>> Rk4_solve(double x0, double y0, double x, double h)
         {
+            // Setup shadow h>0, x0, x>x0 to control loop termination
             double shadow_h = h > 0 ? h : -h;
             double shadow_x0 = x0;
             double shadow_x = h > 0 ? x : ((2 * x0) - x);
-            double halfh = h / 2d;
 
-            double lcl_x = x0;
-            double lcl_x_hh;
-            double lcl_y = y0;
+            // Initialize results List of Lists with initial conditions
+            List<List<double>> results = new List<List<double>>
+            {
+                new List<double>{ x0 },
+                new List<double>{ y0 }
+            };
 
-            List<List<double>> lists = new List<List<double>>();
-            lists.Add(new List<double>());
-            lists.Add(new List<double>());
+            // Initialize values for loop
+            double lcl_x = x0;                  // x
+            double halfh = h / 2d;              // h/2 (effectively constant)
 
-            lists[0].Add(lcl_x);
-            lists[1].Add(lcl_y);
-
-            double esty0, esty1, esty2, esty3;
+            // Intermediate in-loop variables
+            double lcl_y = y0;                  // Estimated y
+            double lcl_x_hh;                    // x + h/2
+            double esty0, esty1, esty2, esty3;  // Misc calcuations for 4th-order R-K
 
             while (shadow_x0 < shadow_x)
             {
-                shadow_x0 += shadow_h;
+                shadow_x0 += shadow_h;  // Setup for loop termination check
 
-                esty0 = h * dydx(lcl_x,                    lcl_y);
+                // Runge-Kutta 4th order model using user-supplied, concrete dydx
+                esty0 = h * dydx(lcl_x,                    lcl_y               );
                 esty1 = h * dydx(lcl_x_hh = lcl_x + halfh, lcl_y + (esty0 / 2d));
                 esty2 = h * dydx(lcl_x_hh,                 lcl_y + (esty1 / 2d));
-                esty3 = h * dydx(lcl_x += h,               lcl_y + esty2);
+                esty3 = h * dydx(lcl_x += h,               lcl_y + esty2       );
 
                 lcl_y += (esty0 + (2d * (esty1 + esty2)) + esty3) / 6d;
 
-                lists[0].Add(lcl_x);
-                lists[1].Add(lcl_y);
+                // Append data to lists
+                results[0].Add(lcl_x);
+                results[1].Add(lcl_y);
             }
-            return lists;
+            return results;
         }
     }
 }
